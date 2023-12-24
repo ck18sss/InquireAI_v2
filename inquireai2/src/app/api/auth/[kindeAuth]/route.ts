1e859182a2a27fcc4267eb8853baacf720c7ab9c
ck18sss <ck18sss@mail.com>
2023-12-24 13:24:39 -0500

    Update route.ts

--- inquireai2/src/app/api/auth/[kindeAuth]/route.ts
+++ inquireai2/src/app/api/auth/[kindeAuth]/route.ts
@@ -1,8 +1,11 @@
-import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
+import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+import { NextRequest } from 'next/server'
 
-export async function GET(request: NextRequest, { params }: any) {
-    const endpoint = params.kindeAuth;
-    const authenticationResponse = await handleAuth(request, endpoint);
-    return authenticationResponse;
+export async function GET(
+  request: NextRequest,
+  { params }: any
+) {
+  const endpoint = params.kindeAuth
+  return handleAuth(request, endpoint)
 }
+
